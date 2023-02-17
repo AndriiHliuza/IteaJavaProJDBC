380a664c3cbec955ae071674cf3cdbc10f712dda
AndriiHliuza <gl093770@gmail.com>
2023-02-17 18:57:50 +0200

    Have added hometask13

---
+++ src/main/java/com/itea/andrii/sql/demo/repository/TableOperations.java
@@ -0,0 +1,9 @@
+package com.itea.andrii.sql.demo.repository;
+
+import java.sql.SQLException;
+
+public interface TableOperations {
+    void createTable() throws SQLException;
+
+    void createForeignKeys() throws SQLException;
+}
